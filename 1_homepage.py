dc0275ad0adcaf40c9c0266fb78f5dc71d79675f
Abdul <abdul_h20@mhs.unsyiah.ac.id>
2023-09-07 15:25:57 +0700

    feat: inital commit to create image processing object

---
+++ 1_homepage.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Pengolahan Citra",
+    page_icon="👋",
+)
+
+
+st.title("Projek Pengolahan Citra")
+st.sidebar.success("Select a page above.")
+
